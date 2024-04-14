64a233ef5440a7e5ec9ce8478dcfc97aae4bbc09
kiko_camba <kikocamba82@gmail.com>
2024-04-14 23:00:35 +0200

    initial

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        message: "Hola mundo"
+    })
+})
+
+app.listen(port, () => {
+    console-log(`Server running on port ${port}`)
+})
